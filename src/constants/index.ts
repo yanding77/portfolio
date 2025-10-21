9f34c1fe3714162889bccbfec6821edf2606c724
yanding77 <yandingvegeta@gmail.com>
2025-10-21 04:25:52 -0400

    w

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -19,11 +19,11 @@ const navLinks = [
 
 const words = [
     { text: "Ideas", imgPath: "./images/ideas.svg" },
-    { text: "Concepts", imgPath: "./images/concepts.svg" },
+    { text: "Plans", imgPath: "./images/concepts.svg" },
     { text: "Designs", imgPath: "./images/designs.svg" },
     { text: "Code", imgPath: "./images/code.svg" },
     { text: "Ideas", imgPath: "./images/ideas.svg" },
-    { text: "Concepts", imgPath: "./images/concepts.svg" },
+    { text: "Plans", imgPath: "./images/concepts.svg" },
     { text: "Designs", imgPath: "./images/designs.svg" },
     { text: "Code", imgPath: "./images/code.svg" },
 ];
