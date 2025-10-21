6aa946fcfe3ce3f2162b254d4fbcf79fcce4b673
yanding77 <yandingvegeta@gmail.com>
2025-10-21 02:42:43 -0400

    M

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
-    base: '/portfolio/',
+    base: '/portfolio',
     plugins: [
       react(),
       tailwindcss(),
