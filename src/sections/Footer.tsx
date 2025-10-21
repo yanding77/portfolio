e4b3942158f4eeab491efe7847471853642fba36
yanding77 <yandingvegeta@gmail.com>
2025-10-21 03:54:34 -0400

    w

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -2,12 +2,16 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
+                <div>
+                </div>
+                <div>
+                </div>
                 <div className="flex flex-col justify-center">
                     <p className="text-center md:text-end">
                         © {new Date().getFullYear()} Yanding Mario Yin Sovenis. All rights reserved
                     </p>
-
                 </div>
+
             </div>
         </footer>
     )
