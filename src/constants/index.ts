a9f9643d3f5cee8859af5f4702eddf017ed4b2c6
yanding77 <yandingvegeta@gmail.com>
2025-10-21 03:24:24 -0400

    w

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -18,14 +18,14 @@ const navLinks = [
 ];
 
 const words = [
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "./images/ideas.svg" },
+    { text: "Concepts", imgPath: "./images/concepts.svg" },
+    { text: "Designs", imgPath: "./images/designs.svg" },
+    { text: "Code", imgPath: "./images/code.svg" },
+    { text: "Ideas", imgPath: "./images/ideas.svg" },
+    { text: "Concepts", imgPath: "./images/concepts.svg" },
+    { text: "Designs", imgPath: "./images/designs.svg" },
+    { text: "Code", imgPath: "./images/code.svg" },
 ];
 
 const counterItems = [
@@ -35,55 +35,20 @@ const counterItems = [
     { value: 3.7, decimals: 1, label: "GPA" },
 ];
 
-const logoIconsList = [
-    {
-        imgPath: "/images/logos/company-logo-1.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-2.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-3.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-4.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-5.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-6.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-7.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-8.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-9.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-10.png",
-    },
-    {
-        imgPath: "/images/logos/company-logo-11.png",
-    },
-];
 
 const abilities = [
     {
-        imgPath: "/images/seo.png",
+        imgPath: "./images/seo.png",
         title: "Quality Focus",
         desc: "Delivering high-quality results while maintaining attention to every detail.",
     },
     {
-        imgPath: "/images/chat.png",
+        imgPath: "./images/chat.png",
         title: "Reliable Communication",
         desc: "Keeping you updated at every step to ensure transparency and clarity.",
     },
     {
-        imgPath: "/images/time.png",
+        imgPath: "./images/time.png",
         title: "On-Time Delivery",
         desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
     },
@@ -92,11 +57,11 @@ const abilities = [
 const techStackImgs = [
     {
         name: "Node.js",
-        imgPath: "/images/logos/node.png",
+        imgPath: "./images/logos/node.png",
     },
     {
         name: "Agile",
-        imgPath: "/images/logos/agile4.png.webp",
+        imgPath: "./images/logos/agile4.png.webp",
     },
 
 ];
@@ -104,20 +69,20 @@ const techStackImgs = [
 const techStackIcons = [
     {
         name: "React",
-        modelPath: "/models/react_logo-transformed.glb",
+        modelPath: "./models/react_logo-transformed.glb",
         scale: 1,
         rotation: [0, 0, 0],
     },
     {
         name: "Python",
-        modelPath: "/models/python-transformed.glb",
+        modelPath: "./models/python-transformed.glb",
         scale: 0.8,
         rotation: [0, 0, 0],
 
     },
     {
         name: "Git",
-        modelPath: "/models/git-svg-transformed.glb",
+        modelPath: "./models/git-svg-transformed.glb",
         scale: 0.05,
         rotation: [0, -Math.PI / 4, 0],
     },
@@ -127,8 +92,8 @@ const expCards = [
     {
         review:
             "WPI Team brought creativity and technical expertise, significantly improving our frontend performance. They carefully listened to our needs and delivered a much more self intuitive interface for staff and patients.",
-        imgPath: "/images/hospital.png",
-        logoPath: "/images/logos/hpLogo.png",
+        imgPath: "./images/hospital.png",
+        logoPath: "./images/logos/hpLogo.png",
         title: "Frontend Developer",
         date: "March 2025 - May 2025",
         responsibilities: [
@@ -142,8 +107,8 @@ const expCards = [
         review:
             "Working with Yanding has been beneficial for our restaurant. He built a web application that allows our guests to order directly from their tables using QR codes. Thanks to his work, we’ve seen a noticeable improvement in efficiency " +
             "and our staff can focus more on customer service rather than manual entry.",
-        imgPath: "/images/logos/balsam.png",
-        logoPath: "/images/logos/balsita.png",
+        imgPath: "./images/logos/balsam.png",
+        logoPath: "./images/logos/balsita.png",
         title: "Full Stack Developer",
         date: "February 2024 - April 2024",
         responsibilities: [
@@ -156,8 +121,8 @@ const expCards = [
     },
     {
         review: "Yanding has a real talent for explaining  technical concepts in a way that’s easy to understand, I really appreciated how he broke down problems step by step, which made assignments and exam prep much clearer.",
-        imgPath: "/images/wpi.png",
-        logoPath: "/images/logos/wpilogo.png",
+        imgPath: "./images/wpi.png",
+        logoPath: "./images/logos/wpilogo.png",
         title: "Undergraduate Teaching Assistant",
         date: "Aug 2024- Present",
         responsibilities: [
@@ -168,36 +133,12 @@ const expCards = [
     },
 ];
 
-const socialImgs = [
-    {
-        name: "insta",
-        url: "https://www.instagram.com/",
-        imgPath: "/images/insta.png",
-    },
-    {
-        name: "fb",
-        url: "https://www.facebook.com/",
-        imgPath: "/images/fb.png",
-    },
-    {
-        name: "x",
-        url: "https://www.x.com/",
-        imgPath: "/images/x.png",
-    },
-    {
-        name: "linkedin",
-        url: "https://www.linkedin.com/",
-        imgPath: "/images/linkedin.png",
-    },
-];
 
 export {
     words,
     abilities,
-    logoIconsList,
     counterItems,
     expCards,
-    socialImgs,
     techStackIcons,
     techStackImgs,
     navLinks,
